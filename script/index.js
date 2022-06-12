c16b36ca5e4d5847838a66e08b744b74a4043fae
Konstantin Nivnikov <nivnikoff@gmail.com>
2022-06-12 13:03:48 +0300

    fix: post-review fixes 2

--- script/index.js
+++ script/index.js
@@ -2,14 +2,12 @@ let editButton = document.querySelector('.profile__edit-button');
 let popupEdit = document.querySelector('.popup-edit');
 let closeButton = document.querySelector('.popup-edit__close-button');
 let formElement = document.querySelector('.popup-edit__form');
-let nameInput = document.querySelector('.popup-edit__input-text_name');
-let descriptionInput = document.querySelector('.popup-edit__input-text_description');
+let nameInput = document.querySelector('.popup-edit__input-text_type_name');
+let descriptionInput = document.querySelector('.popup-edit__input-text_type_description');
 let nameProfile = document.querySelector(".profile__name");
 let descriptionProfile = document.querySelector(".profile__description");
 
 function openPopup() {
-  nameProfile.textContent;
-  descriptionProfile.textContent;
   nameInput.value = nameProfile.textContent;
   descriptionInput.value = descriptionProfile.textContent;
   popupEdit.classList.add('popup-edit_opened');
@@ -21,8 +19,6 @@ function closePopup() {
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  nameInput.value;
-  descriptionInput.value;
   nameProfile.textContent = nameInput.value;
   descriptionProfile.textContent = descriptionInput.value;
   popupEdit.classList.remove('popup-edit_opened');
