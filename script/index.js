59b80a28acbf1c92906662d08dc08dd73b266f0f
Konstantin Nivnikov <nivnikoff@gmail.com>
2022-06-22 18:46:32 +0300

    feature: add pop-up layout added

--- script/index.js
+++ script/index.js
@@ -1,20 +1,31 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popupEdit = document.querySelector('.popup-edit');
-let closeButton = document.querySelector('.popup-edit__close-button');
+let closeEditButton = document.querySelector('.popup-edit__close-button');
 let formElement = document.querySelector('.popup-edit__form');
 let nameInput = document.querySelector('.popup-edit__input-text_type_name');
 let descriptionInput = document.querySelector('.popup-edit__input-text_type_description');
 let nameProfile = document.querySelector(".profile__name");
 let descriptionProfile = document.querySelector(".profile__description");
+let addButton = document.querySelector('.profile__add-button');
+let popupAdd = document.querySelector('.popup-add');
+let closeAddButton = document.querySelector('.popup-add__close-button');
 
-function openPopup() {
+function openPopupEdit() {
   nameInput.value = nameProfile.textContent;
   descriptionInput.value = descriptionProfile.textContent;
   popupEdit.classList.add('popup-edit_opened');
 }
 
-function closePopup() {
+function closePopupEdit() {
   popupEdit.classList.remove('popup-edit_opened');
+}
+
+function openPopupAdd() {
+  popupAdd.classList.add('popup-add_opened');
+}
+
+function closePopupAdd() {
+  popupAdd.classList.remove('popup-add_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -24,6 +35,8 @@ function formSubmitHandler (evt) {
   popupEdit.classList.remove('popup-edit_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopupEdit);
+closeEditButton.addEventListener('click', closePopupEdit);
 formElement.addEventListener('submit', formSubmitHandler);
+addButton.addEventListener('click', openPopupAdd);
+closeAddButton.addEventListener('click', closePopupAdd);
